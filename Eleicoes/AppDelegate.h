03489e237a908bdfdbf36459c9ec85ca0e1e4f53
Katha <katha@Kathas-MacBook-Pro.local>
2014-08-19 21:05:12 -0300

    Initial Commit

---
+++ Eleicoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Eleicoes
+//
+//  Created by Katha on 19.08.14.
+//  Copyright (c) 2014 Nappix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
